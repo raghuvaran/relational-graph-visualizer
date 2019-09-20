4ff9ac55444e394765f1e3f51a76068734979bd8
Nick Hwang <Hokyung.Hwang@Honeywell.com>
2019-09-20 14:28:14 -0400

    added d3 graph format

--- sql-visualizer-backend/src/index.ts
+++ sql-visualizer-backend/src/index.ts
@@ -1,6 +1,51 @@
 import 'reflect-metadata';
 import { createConnection } from 'typeorm';
 
+type StringGraph = Map<string, Set<string>>;
+
+/**
+ * Make a undirected graph and create a connection both ways
+ * @param graph Graph
+ */
+const connectEdges = (graph: StringGraph) => {
+  for (const key of graph.keys()) {
+    for (const v of graph.get(key).values()) {
+      graph.get(v).add(key);
+    }
+  }
+};
+
+const buildGraph = (mapping) => {
+  const graph = new Map<string, Set<string>>();
+
+  mapping.forEach((mapped) => {
+    graph.set(mapped.primary, new Set(mapped.foreign));
+  });
+  connectEdges(graph);
+  return graph;
+};
+
+const buildD3Graph = (graph: StringGraph) => {
+  const nodes = [...graph.keys()].map((key) => ({ id: key, group: 1 }));
+  const links = [];
+  const visited = new Set<string>();
+  for (const u of graph.keys()) {
+    for (const v of graph.get(u).values()) {
+      const edgeKey = `${u},${v}`;
+      const reverseEdgeKey = `${v},${u}`;
+      if (!visited.has(edgeKey) && !visited.has(reverseEdgeKey)) {
+        links.push({
+          source: u,
+          target: v,
+          value: 50
+        });
+      }
+    }
+  }
+  return { nodes, links };
+  // dfs
+};
+
 createConnection()
   .then(async (connection) => {
     console.log('Here you can setup and run express/koa/any other framework.');
@@ -11,32 +56,47 @@ createConnection()
     const res = await connection.query('show tables');
     const tables = res.map((table) => table.table_name);
     const edges = [];
+    const mapping = [];
     for (const table of tables) {
       const tableName = keywordSet.has(table) ? `"${table}"` : table;
       const constraints = await connection.query(
         `show constraints from ${tableName}`
       );
-      const constraintsFilteredByForeginAndPrimary = constraints.filter(
+      const constraintsFilteredByForeignAndPrimary = constraints.filter(
         (constraint) =>
           constraint.constraint_type.indexOf('FOREIGN') !== -1 ||
           constraint.constraint_type.indexOf('PRIMARY') !== -1
       );
 
-      const constrainsMapping = constraintsFilteredByForeginAndPrimary.map(
-        (constraint) =>
-          constraint.details.indexOf('FOREIGN') !== -1
-            ? {
-                // TODO hard coding right now change it to something else
-                foreign: constraint.details.split(' ')[4]
-              }
-            : { primary: constraint.table_name }
-      );
-      console.log(constrainsMapping);
-
+      // console.log(constraintsFilteredByForeignAndPrimary);
+      // console.log(constraintsFilteredByForeignAndPrimary.length);
+      // initialize array
+      const constraintMap = { primary: '', foreign: [] };
+      // console.log(constraintArr);
+      constraintsFilteredByForeignAndPrimary.forEach((constraint) => {
+        // console.log('before', {
+        //   index,
+        //   details: constraint.details,
+        //   obj: constraintArr[index]
+        // });
+        if (constraint.details.indexOf('FOREIGN') !== -1) {
+          constraintMap.foreign.push(constraint.details.split(' ')[4]);
+        } else {
+          constraintMap.primary = tableName;
+        }
+        // console.log('after', {
+        //   obj: constraintArr[index]
+        // });
+      });
+      // console.log(constraintMap);
+      mapping.push(constraintMap);
+      // buildGraph(constraintsMapping);
       // constraints.map((constraint) => console.log(constraint));
       // edges.push(constraints.map((constraint) => console.log(constraint))
     }
-    console.log(edges);
+    const graph = buildGraph(mapping);
+    const d3Graph = buildD3Graph(graph);
+    console.log(JSON.stringify(d3Graph, null, 2));
     // // console.log("Inserting a new user into the database...");
     // // const user = new User();
     // // user.firstName = "Timber";
@@ -51,5 +111,3 @@ createConnection()
     connection.close();
   })
   .catch((error) => console.log(error));
-
-// constraint.filter((con) => con.constraint_type.toLocaleLowerCase().indexOf('foreign') !== -1 || con.constraint_type.toLocalLowerCase().indexOf('primary') !== -1)));
