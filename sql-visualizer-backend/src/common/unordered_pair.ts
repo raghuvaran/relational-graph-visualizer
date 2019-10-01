70e78f39732fb13ce5db2e0b5947a37ac967d4e8
Raghuvaran Chowda <raghuvaran.chowda@honeywell.com>
2019-10-01 14:16:44 -0400

    Cleanup unused utils

--- sql-visualizer-backend/src/common/unordered_pair.ts
+++
@@ -1,8 +0,0 @@
-import { AbstractPair } from './abstract_pair';
-
-export class UnorderedPair<T> extends AbstractPair<T> {
-  equals(otherPair: AbstractPair<T>): boolean {
-    return (this.u === otherPair.u && this.v === otherPair.v) ||
-      (this.u === otherPair.v && this.v === otherPair.u);
-  }
-}
